f3e425590177137303b4d992594c7b37eefd9681
Federico Manes <federico.manes@globant.com>
2025-04-21 03:14:01 -0300

    chore: rename logger file

--- crud_challenge/util_logger.py
+++ crud_challenge/util_logger.py
@@ -5,5 +5,4 @@ logging.basicConfig(
     level=logging.INFO,  # You can change to DEBUG, WARNING, ERROR, etc.
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
-
 logger = logging.getLogger(__name__)
