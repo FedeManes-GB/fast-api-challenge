5954edc1ffc52837ee59edfb4358fa6baa8d83bc
Federico Manes <federico.manes@globant.com>
2025-04-21 01:04:19 -0300

    chore: added response model

--- crud_challenge/main.py
+++ crud_challenge/main.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 
 from .database import SessionLocal, engine
 from .models_db import Base, User
-from .models_pydantic import UserRequest, UserUpdate
+from .models_pydantic import UserCreate, UserRequest, UserUpdate
 
 app = FastAPI()
 
@@ -24,12 +24,12 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/", status_code=status.HTTP_200_OK)
+@app.get("/users", status_code=status.HTTP_200_OK, response_model=list[UserCreate])
 def list_users(db: db_dependency):
     return db.query(User).all()
 
 
-@app.get("/users/{id}", status_code=status.HTTP_200_OK)
+@app.get("/users/{id}", status_code=status.HTTP_200_OK, response_model=UserCreate)
 def get_user(db: db_dependency, id: int = Path(gt=0)):
     find_user = db.query(User).filter(User.id == id).first()
     if find_user is not None:
@@ -38,7 +38,9 @@ def get_user(db: db_dependency, id: int = Path(gt=0)):
         raise HTTPException(status_code=404, detail="User not found")
 
 
-@app.post("/users/create", status_code=status.HTTP_201_CREATED)
+@app.post(
+    "/users/create", status_code=status.HTTP_201_CREATED, response_model=UserCreate
+)
 def create_user(db: db_dependency, input_user: UserRequest):
     existing_user = db.query(User).filter(User.email == input_user.email).first()
     if existing_user:
@@ -48,9 +50,15 @@ def create_user(db: db_dependency, input_user: UserRequest):
     new_user = User(id=new_id, **input_user.model_dump())
     db.add(new_user)
     db.commit()
+    db.refresh(new_user)
+    created_user = db.query(User).filter(User.id == new_id).first()
+    return created_user
 
 
-@app.put("/users/update/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.put(
+    "/users/update/{id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
 def update_users(db: db_dependency, update_user: UserUpdate, id: int = Path(gt=0)):
     user_to_update = db.query(User).filter(User.id == id).first()
     if user_to_update is not None:
@@ -60,6 +68,9 @@ def update_users(db: db_dependency, update_user: UserUpdate, id: int = Path(gt=0
                 setattr(user_to_update, key, value)
         db.add(user_to_update)
         db.commit()
+        db.refresh(user_to_update)
+        updated_user = db.query(User).filter(User.id == id).first()
+        return updated_user
     else:
         raise HTTPException(status_code=404, detail="User not found")
 
